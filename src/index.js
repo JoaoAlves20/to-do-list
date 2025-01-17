fd31510d51cf01d975b054a34f5f79982403b9b6
JoaoAlves20 <jvsimoesalves@gmail.com>
2025-01-17 19:24:45 -0300

    feat: created the button logic and new styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const input = document.querySelector(".input");
 const button = document.querySelector(".button");
 const toDoList = document.querySelector(".to-do-list");
+const form = document.querySelector(".container");
 
 function createElement(tag, className) {
     const element = document.createElement(tag);
@@ -8,15 +9,26 @@ function createElement(tag, className) {
     return element;
 };
 
-function createToDoList() {
-    const valueInput = input.value;
+function validationInput({ target }) {
+    if (target.value.length > 2) {
+        button.removeAttribute("disabled");
+        return;
+    };
+
+    button.setAttribute("disabled", "");
+};
+
+function createToDoList(event) {
+    event.preventDefault();
     
+    const valueInput = input.value;
+
     const createDiv = createElement("div", "container-response");
     const createText = createElement("span", "span-created");
     const createButton = createElement("button", "button-created");
 
     createText.innerHTML = valueInput;
-    createButton.innerHTML = "OK";
+    createButton.innerHTML = "Finalizar";
 
     createButton.addEventListener("click", () => toDoList.removeChild(createDiv));
 
@@ -26,6 +38,9 @@ function createToDoList() {
     toDoList.appendChild(createDiv);
 
     input.value = "";
+
+    button.setAttribute("disabled", "");
 };
 
-button.addEventListener("click", createToDoList);
+input.addEventListener("input", validationInput);
+form.addEventListener("submit", createToDoList);
